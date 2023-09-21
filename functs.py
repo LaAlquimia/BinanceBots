fbdc36281f93255d7d537049804237fed8ca42ca
laalquimia <laalquimia@skynet.localdomain>
2023-09-21 17:13:24 -0500

    REVEALED VOLUME

--- functs.py
+++ functs.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 async def processKlines(client, _symbol):
-    kline =client.futures_klines(symbol= _symbol, interval =  client.KLINE_INTERVAL_5MINUTE, limit = 288)
+    kline =client.futures_klines(symbol= _symbol, interval =  client.KLINE_INTERVAL_5MINUTE, limit = 20)
     df = pd.DataFrame(pd.to_numeric([x[5] for x in kline]))
     prev = df.tail(3).iloc[1] 
     mean_volume = df.mean() 
-    value = ((prev- mean_volume)/ prev).values[0]
-    if value > 1:
-        print("Alert in :" , _symbol) 
+    value = ((prev- mean_volume)/ mean_volume).values[0]
+    if value> 0.45:
+        print(_symbol , value) 
     return 
 
 async def filtrarVol():
